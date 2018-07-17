81380908fd0d050b8448c2f5d1ded1a010cd0c0d
Lara Sanders <larasanders@gmail.com>
2018-07-17 18:45:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,53 @@
+function getRepositories() {
+  const req = new XMLHttpRequest()
+   req.addEventListener("load", displayRepositories);
+  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  req.send()
+}
+
+function displayRepositories() {
+  const repos = JSON.parse(this.responseText)
+  const repoList = "<ul>" + repos.map(r => {
+    const dataUsername = 'data-username="' + r.owner.login + '"'
+    const dataRepoName = 'data-repository="' + r.name + '"'
+    return(`
+          <li>
+            <h2>${r.name}</h2>
+            <a href="${r.html_url}">${r.html_url}</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a>
+          </li>`)
+  }).join('') + "</ul>";
+  document.getElementById("repositories").innerHTML = repoList
+}
+
+function getCommits(el) {
+  const name = el.dataset.repository
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", displayCommits)
+  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
+  req.send()
+
+}
+
+function displayCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.commit.author.name + '</strong> - '  +  commit.author.login  + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("details").innerHTML = commitsList
+
+}
+
+function getBranches(el) {
+  const name = el.dataset.repository
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", displayBranches)
+  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/branches')
+  req.send()
+}
+
+function displayBranches() {
+  const branches = JSON.parse(this.responseText)
+  const branchesList = `<ul>${branches.map(branch => '<li><strong>' + branch.name + '</strong></li>').join('')}</ul>`
+  document.getElementById("details").innerHTML = branchesList
+
+}
